3c3640df417453f018f774c0a77076ef5eec5784
Benjamin <benjaxd089@gmail.com>
2022-08-24 20:20:28 -0400

    Hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'menutabs',
+    loadChildren: () => import('./menutabs/menutabs.module').then( m => m.MenutabsPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
 ];
 
 @NgModule({
